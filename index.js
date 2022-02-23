0a4a940a36a268cef23a97e42653c68637746305
Kira272921 <sunset272921@gmail.com>
2022-02-23 12:21:02 +0530

    feat: use json

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
 const cors = require('cors')
 const express = require('express')
-const dotenv = require('dotenv')
-const mongoinit = require('./database/mongo')
-const links = require('./database/schemas/links')
-const projects = require('./database/schemas/projects')
+const data = require('./links.json')
+const config = require('./config.json')
 
-dotenv.config()
 const app = express()
-mongoinit(process.env.MONGODB_URL)
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', async (req, res) => {
-    res.redirect('https://kiradev.co')
+    res.redirect(`${config.BASE_URL}`)
 })
 
 app.get('/:name', async (req, res) => {
-    const slugLink = await links.findOne({ slug: req.params.name })
-    if (slugLink === null) {
-        res.redirect('https://kiradev.co')
+    const slugLink = data["social"][req.params.name]
+    if (slugLink === undefined) {
+        res.redirect(`${config.BASE_URL}`)
     }
     else {
-        res.redirect(slugLink.link)
+        res.redirect(slugLink)
     }
 })
 
 app.get('/project', async (req, res) => {
-    res.redirect('https://www.kiradev.co/projects')
+    res.redirect(`${config.PROJECTS_URL}`)
 })
 
 app.get('/project/:name', async (req, res) => {
-    const slugLink = await projects.findOne({ slug: req.params.name })
-    if (slugLink === null) {
-        res.redirect('https://kiradev.co')
+    const slugLink = data["projects"][req.params.name]
+    if (slugLink === undefined) {
+        res.redirect(`${config.BASE_URL}`)
     }
     else {
-        res.redirect(slugLink.link)
+        res.redirect(slugLink)
     }
 })
 
